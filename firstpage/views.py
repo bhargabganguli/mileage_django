f3cd724aede79e0be812d386908fec075c4b47a4
bhargabganguli <89978856+bhargabganguli@users.noreply.github.com>
2022-03-15 11:49:56 +0530

    Update views.py

--- firstpage/views.py
+++ firstpage/views.py
@@ -34,7 +34,7 @@ def area_plot(request):
     
     weights = weights.squeeze()
     base = lr.intercept_
-    """
+    
     unadj_contributions = x_data.mul(weights).assign(Base=base)
     
     adj_contributions = (unadj_contributions.div(unadj_contributions.sum(axis=1), axis=0).mul(y_data, axis=0)) # contains all contributions for each day
@@ -47,7 +47,7 @@ def area_plot(request):
     buffer.seek(0)
     string = base64.b64encode(buffer.read())
     uri = urllib.parse.quote(string)     
-    """
+    
     return render(request, 'mmm.html', {'x':weights})
 
 def imp_features(request):

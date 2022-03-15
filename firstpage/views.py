a67ea6616463f0af243b3be0dea01e8dd537abc0
bhargabganguli <89978856+bhargabganguli@users.noreply.github.com>
2022-03-15 18:44:12 +0530

    Update views.py

--- firstpage/views.py
+++ firstpage/views.py
@@ -34,10 +34,11 @@ def area_plot(request):
     #weight = pd.Series(np.array([2,3,4,5]),index=X.columns)
     base = lr.intercept_
     coeff = lr.coef_
-    weight = pd.Series(coeff.squeeze())
+    #weight = pd.Series(coeff.squeeze())
     
-    unadj_contributions = x_data.mul(weight).assign(Base=base)
-    """
+    #unadj_contributions = x_data.mul(weight).assign(Base=base)
+    unadj_contributions = x_data
+    
     adj_contributions = (unadj_contributions.div(unadj_contributions.sum(axis=1), axis=0).mul(y_data, axis=0)) # contains all contributions for each day
     ax = (adj_contributions[['Base', 'cyl', 'disp', 'wt', 'acc']].plot.area(figsize=(16, 10),linewidth=1,title='Predicted Sales and Breakdown',ylabel='Sales',xlabel='Date'))
     handles, labels = ax.get_legend_handles_labels()
@@ -48,8 +49,8 @@ def area_plot(request):
     buffer.seek(0)
     string = base64.b64encode(buffer.read())
     uri = urllib.parse.quote(string)     
-    """
-    return render(request, 'mmm.html', {'x':unadj_contributions})
+    
+    return render(request, 'mmm.html', {'x':uri})
 
 def imp_features(request):
         uri=imp()

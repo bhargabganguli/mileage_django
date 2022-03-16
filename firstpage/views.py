600202553f98d8e501043b4fd680f8afddcc5515
bhargabganguli <89978856+bhargabganguli@users.noreply.github.com>
2022-03-17 00:00:38 +0530

    Update views.py

--- firstpage/views.py
+++ firstpage/views.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score, TimeSeriesSplit
 # Create your views here.
 
+
 #this will start first page
 from sklearn import pipeline,preprocessing,metrics,model_selection,ensemble
 #from sklearn_pandas import DataFrameMapper

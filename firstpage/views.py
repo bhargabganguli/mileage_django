3da2ce9ef19ad4b3792ec37aa2e58f4f2fafd027
bhargabganguli <89978856+bhargabganguli@users.noreply.github.com>
2022-03-18 13:43:12 +0530

    Update views.py

--- firstpage/views.py
+++ firstpage/views.py
@@ -205,7 +205,7 @@ def area_plot(request):
     uri = uri.decode('utf-8')
     buffer.close()
     
-    return render(request, 'mmm.html',{'x':adj_contributions})    
+    return render(request, 'mmm.html',{'x':uri})    
     
     
 #this is user defined function to load the csv data into a  dataframe(name=csv) and to upload it in mysql database
